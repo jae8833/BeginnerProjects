b707825103eafbb66d7034a287e08bf28a61e9e3
Jae Hyeon Lee <jaelee8210@gmail.com>
2022-09-17 10:41:17 -0700

    madlibs

---
+++ madlibs/madlibs.py
@@ -0,0 +1,8 @@
+# get inputs from user
+name = input("Name: ")
+adj = input("Adjective: ")
+noun = input("Noun: ")
+verb = input("Verb: ")
+
+# prints a madlib story
+print(f"{name} decided go on a walk. {name} was ecstatic to see the {adj} {noun} {verb}.")
